282535630710c28c7efc1bfc3a3e9e785397a786
fleriking <fleriking@gmail.com>
2023-05-08 08:18:11 +0300

    fix: chek folder in 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const { readdir, readFile } = require('fs/promises');
+const { readdir, readFile, stat } = require('fs/promises');
 const fs = require('fs');
 
-const DIST_DIR = path.join(__dirname, 'project-dist');
 const DIST_BUNDLE = path.join(__dirname, 'project-dist', 'bundle.css');
 const COPY_CSS = path.join(__dirname, 'styles');
 
@@ -15,15 +14,21 @@ function bundle(filePath, fromDir) {
     .then(files => {
 
       files.forEach(file => {
-        if (path.extname(file.name) == '.css') {
-          readFile(path.join(COPY_CSS, file.name),{encoding: 'utf-8'})
-            .then(fileContent => {
+        stat(path.join(COPY_CSS, file.name))
+        .then(stats =>{
 
-              fs.appendFile(filePath, fileContent,'utf-8', err => { });
-              fs.appendFile(filePath, '\n','utf-8', err => { });
-            })
-
-        }
+          if (path.extname(file.name) == '.css' &&
+            stats.isFile()) {
+  
+            readFile(path.join(COPY_CSS, file.name), { encoding: 'utf-8' })
+              .then(fileContent => {
+  
+                fs.appendFile(filePath, fileContent, 'utf-8', err => { });
+                fs.appendFile(filePath, '\n', 'utf-8', err => { });
+              })
+  
+          }
+        })
       })
     })
 }
